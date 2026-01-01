0f22b14d7c53d94e9e38656f99fafe24f39474b9
naoki-wjm <naoki_wjm@yumegatari.com>
2026-01-01 12:50:52 +0900

    チュートリアルページjs対策

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   optimizeDeps: {
     exclude: ['sweph-wasm']
   },
-  assetsInclude: ['**/*.wasm']
+  assetsInclude: ['**/*.wasm'],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        tutorial: resolve(__dirname, 'tutorial.html'),
+      },
+    },
+  },
 })
